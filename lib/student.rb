2da112a8c1424d3736dddba27a6f7f57d2de0b06
Charles Pelton <charliejp0311@gmail.com>
2020-03-24 21:10:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,9 +34,13 @@ class Student
         VALUES (?,?)
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+      #@id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
     end
 
+  end
+
+  def self.create 
+    
   end
 
   def update
