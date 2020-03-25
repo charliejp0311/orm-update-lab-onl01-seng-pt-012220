94f9d389e44e020be69a38cca0fc3369a50841af
Charles Pelton <charliejp0311@gmail.com>
2020-03-25 14:11:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,10 +46,7 @@ class Student
 
   def self.new_from_db(row)
     # binding.pry
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
+    new_student = self.new(row[1], row[2], row[0])
   end
 
   def update
